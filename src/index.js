0cbc049cf02f029112cb05f065a703c148f764ff
Dmytro Kravchenko <dmytrokravchenko97@gmail.com>
2025-10-07 15:46:57 +0300

    v. 0.3.2

--- src/index.js
+++ src/index.js
@@ -3,15 +3,3 @@ import "./js/simple";
 import "./js/authorization";
 import "./js/validation";
 import "./js/market";
-
-import iconsUrl from './images/icons.svg';
-
-console.log(iconsUrl);
-
-document.querySelectorAll('use').forEach((use) => {
-    const href = use.getAttribute('href');
-    if (href.startsWith('./images/icons.svg')) {
-        const id = href.split('#')[1];
-        use.setAttribute('href', `${iconsUrl}#${id}`);
-    }
-});
