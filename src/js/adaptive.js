171550db38544c4c0f49f091ad73790b925cb4d8
Dmytro Kravchenko <dmytrokravchenko97@gmail.com>
2025-10-12 19:42:47 +0300

    v. 0.5

--- src/js/adaptive.js
+++ src/js/adaptive.js
@@ -1,6 +1,4 @@
-import { header, 
-    headerMob,
-    marketTokensList,
+import { marketTokensList,
     marketTokensListMob,
     marketTokensGrid,
     marketTokensGridMob, } from './refs';
@@ -10,15 +8,11 @@ const deskWidth = 1439;
 const browserWidth = window.innerWidth;
 
 if (browserWidth <= deskWidth) {
-    addVisuallyHidden(header);
-    removeVisuallyHidden(headerMob);
     addVisuallyHidden(marketTokensList);
     removeVisuallyHidden(marketTokensListMob);
     addVisuallyHidden(marketTokensGrid);
     removeVisuallyHidden(marketTokensGridMob);
 } else {
-    addVisuallyHidden(headerMob);
-    removeVisuallyHidden(header);
     addVisuallyHidden(marketTokensListMob);
     removeVisuallyHidden(marketTokensList);
     addVisuallyHidden(marketTokensGridMob);
